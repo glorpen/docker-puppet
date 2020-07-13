78a74ffebd02ce815b7eee0789179e23ae1768b5
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2020-07-13 16:48:56 +0200

    cli: add more env's and help texts

--- glorpen_entrypoint/cli.py
+++ glorpen_entrypoint/cli.py
@@ -36,16 +36,16 @@ def add_vault_auth_arguments(parser):
     parser.add_argument('--vault-app-role', default=os.environ.get("VAULT_APP_ROLE", None))
     parser.add_argument('--vault-app-secret', default=os.environ.get("VAULT_APP_SECRET", None))
 
-    parser.add_argument('--vault-client-cert', type=str, default=None)
-    parser.add_argument('--vault-client-key', type=str, default=None)
-    parser.add_argument('--vault-server-cert', type=str, default=None)
+    parser.add_argument('--vault-client-cert', type=str, default=os.environ.get("VAULT_CLIENT_CERT", None))
+    parser.add_argument('--vault-client-key', type=str, default=os.environ.get("VAULT_CLIENT_KEY", None))
+    parser.add_argument('--vault-server-cert', type=str, default=os.environ.get("VAULT_SERVER_CERT", None))
 
 def add_vault_cert_arguments(parser, cn=True):
-    parser.add_argument('--path', default=os.environ.get("VAULT_PATH", "pki"))
-    parser.add_argument('--role', default=os.environ.get("VAULT_ROLE"))
+    parser.add_argument('--path', default=os.environ.get("VAULT_PATH", "pki"), help="Vault backend mountpoint")
+    parser.add_argument('--role', default=os.environ.get("VAULT_ROLE"), help="Vault role to use")
     if cn:
         parser.add_argument('--cn', default=os.environ.get("VAULT_CN"))
-    parser.add_argument('--lease-ttl', default=os.environ.get("VAULT_LEASE_TTL"), type=type_timedelta)
+    parser.add_argument('--lease-ttl', default=os.environ.get("VAULT_LEASE_TTL"), type=type_timedelta, help="Time until certificate expiration, eg. 2w(eeks), 1d10m3s")
 
 def add_catchall_argument(parser):
     parser.add_argument('args', nargs='*')
