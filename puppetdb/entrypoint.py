9cd468df4060470432cec2fadb444965554b52c6
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2020-07-13 16:48:07 +0200

    puppetdb: set cert cn from CERTNAME variable

--- puppetdb/entrypoint.py
+++ puppetdb/entrypoint.py
@@ -12,19 +12,20 @@ from glorpen_entrypoint.renderer import Renderer, Runner
 p = argparse.ArgumentParser("glorpen-entrypoint")
 
 glorpen_entrypoint.cli.add_vault_auth_arguments(p)
-glorpen_entrypoint.cli.add_vault_cert_arguments(p)
+glorpen_entrypoint.cli.add_vault_cert_arguments(p, cn=False)
 glorpen_entrypoint.cli.add_catchall_argument(p)
 glorpen_entrypoint.cli.add_verbosity_argument(p)
 
 ns = p.parse_args()
+cn = os.environ["CERTNAME"]
 
 logging.basicConfig(level=glorpen_entrypoint.cli.get_verbosity(ns.verbose))
 
-watcher = CertWatcher(ns.path, ns.role, ns.cn, ns.lease_ttl)
+watcher = CertWatcher(ns.path, ns.role, cn, ns.lease_ttl)
 renderer = Renderer(
     "/opt/puppetlabs/server/data/puppetdb/certs/certs/ca.pem",
-    "/opt/puppetlabs/server/data/puppetdb/certs/certs/puppetdb.pem",
-    "/opt/puppetlabs/server/data/puppetdb/certs/private_keys/puppetdb.pem"
+    f"/opt/puppetlabs/server/data/puppetdb/certs/certs/{cn}.pem",
+    f"/opt/puppetlabs/server/data/puppetdb/certs/private_keys/{cn}.pem"
 )
 
 def reload(proc):
